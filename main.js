15ac1c78536b1ae45f1656ba0cf55b3fcc0cba7e
Julie Ng <me@julie.io>
2022-02-02 09:23:24 +0100

    🤦‍♀️ async/await

--- main.js
+++ main.js
@@ -3,23 +3,27 @@
 const core = require('@actions/core')
 const linter = require('./action/')
 
-try {
-  const path = core.getInput('path')
-	const results = linter.run({ path: path })
-  core.setOutput('linted', results.all)
-  console.log('results')
-  console.log(results)
+async function run () {
+  try {
+    const path = core.getInput('path')
+    const results = await linter.run({ path: path })
+    core.setOutput('linted', results.all)
+    console.log('results')
+    console.log(results)
 
-  if (results.errors && results.errors.length > 0) {
-    core.setOutput('errors', JSON.stringify(results.errors))
-    // core.setOutput('comment-url', results.commentUrl)
-    core.setOutput('comment-url', 'Should have errors here in failing case')
+    if (results.errors && results.errors.length > 0) {
+      core.setOutput('errors', JSON.stringify(results.errors))
+      // core.setOutput('comment-url', results.commentUrl)
+      core.setOutput('comment-url', 'Should have errors here in failing case')
+      process.exit(1)
+    } else {
+      core.setOutput('errors', '[]')
+      core.setOutput('comment-url', '')
+    }
+  } catch (err) {
+    console.log(err)
     process.exit(1)
-  } else {
-    core.setOutput('errors', '[]')
-    core.setOutput('comment-url', '')
   }
-} catch (err) {
-  console.log(err)
-  process.exit(1)
 }
+
+run()
