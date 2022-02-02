af79629a18794b6a18644f85a71054d91ec1130f
Julie Ng <me@julie.io>
2022-02-02 09:21:27 +0100

    why are outputs not updated?

--- main.js
+++ main.js
@@ -7,10 +7,13 @@ try {
   const path = core.getInput('path')
 	const results = linter.run({ path: path })
   core.setOutput('linted', results.all)
+  console.log('results')
+  console.log(results)
 
   if (results.errors && results.errors.length > 0) {
     core.setOutput('errors', JSON.stringify(results.errors))
-    core.setOutput('comment-url', results.commentUrl)
+    // core.setOutput('comment-url', results.commentUrl)
+    core.setOutput('comment-url', 'Should have errors here in failing case')
     process.exit(1)
   } else {
     core.setOutput('errors', '[]')
